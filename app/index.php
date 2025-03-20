895e8d4d21479cbb60aacf96acc1cdc32406c969
João Pedro <joao.galvao25@etec.sp.gov.br>
2025-03-20 01:50:31 +0000

    Aula_1903_2025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,43 @@
 <?php
-// require './vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// use Application\DBConnection\MySQLConnection;
+use Slim\Views\PhpRenderer;
 
-// $db = new MySQLConnection();
+require __DIR__ . '/vendor/autoload.php';
 
-echo phpinfo();
+$app = AppFactory::create();
+
+$app->get('/', 
+    function(
+        Request $request, 
+        Response $response, 
+        $args
+    ) {
+        $response->getBody()->write("Hello World!!!");
+        return $response;
+});
+
+$app->get('/mensagem/{nome}', 
+function(
+        Request $request, 
+        Response $response, 
+        $args
+    ) {
+        $response->getBody()->write("Good Night " . $args['nome']);
+         return $response;
+});
+
+$app->get('/hello', 
+function(
+        Request $request, 
+        Response $response, 
+        $args
+    ) {
+        $renderer = new PhpRenderer(__DIR__ . '/templates');
+        $msg = 'New Message!';
+        return $renderer->render($response, 'hello.php', ['mensagem' => $msg]);
+});
+
+$app->run(); 
---
+++ app/templates/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> Hello </title>
+    </head>
+    <body>
+        <h1> It works, this is beautiful! <?= $mensagem ?> </h1>
+    </body>
+</html>
